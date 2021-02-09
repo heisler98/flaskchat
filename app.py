44573c14b1c59581a099582043f65c925928a35e
colingoodman <colin@colingoodman.net>
2021-02-08 22:01:44 -0600

    simple user auth, db hookup

--- app.py
+++ app.py
@@ -2,8 +2,15 @@
 
 from flask import Flask, render_template, request, redirect
 from flask_socketio import SocketIO, join_room
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+
+from db import get_user
 
 app = Flask(__name__)
+app.secret_key = "my secret key"
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 socketio = SocketIO(app)
 
 
@@ -12,7 +19,33 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:  # prevents user from logging in again
+        return redirect('/')
+    message = ''
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password_input = request.form.get('password')
+        user = get_user(username)
+
+        if user and user.check_password(password_input):
+            login_user(user)
+            return redirect('/')
+        else:
+            message = 'Failed to login.'
+    return render_template('login.html', message=message)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/')
+
+
 @app.route('/chat')
+@login_required
 def chat():
     print('chat')
 
@@ -38,6 +71,11 @@ def handle_join_room_event(data):
     app.logger.info("{} has joined the room {}".format(data['username'], data['room']))
     join_room(data['room'])
     socketio.emit('join_room_announcement', data)
+
+
+@login_manager.user_loader
+def load_user(username):
+    return get_user(username)
 
 
 if __name__ == '__main__':
---
+++ db.py
@@ -0,0 +1,24 @@
+# github.com/colingoodman
+
+from pymongo import MongoClient
+from werkzeug.security import generate_password_hash
+
+from user import User
+
+client = MongoClient('mongodb+srv://user:SecurePassword@cluster0.ojjqo.mongodb.net/<dbname>?retryWrites=true&w=majority')
+
+chat_db = client.get_database('ChatDB')
+users_collection = chat_db.get_collection('users')
+
+
+def save_user(username, email, password):
+    password_hash = generate_password_hash(password)
+    users_collection.insert_one({'_id': username, 'email': email, 'password': password_hash})
+
+
+def get_user(username):
+    user_data = users_collection.find_one({'_id': username})
+    return User(user_data['_id'], user_data['email'], user_data['password']) if user_data else None
+
+
+#save_user('colin', 'c@c.c', 'test')
