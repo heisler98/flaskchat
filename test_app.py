ec2edcb605e8c0e02ef68f63ca33afe03cea6579
colingoodman <colin@colingoodman.net>
2021-05-16 19:13:58 -0500

    figured out how testing works

--- test_app.py
+++ test_app.py
@@ -3,27 +3,29 @@ import pytest
 from app import app as flask_app
 import json
 
-
-@pytest.fixture
-def app():
-    yield flask_app
+token = ""
 
 
 @pytest.fixture
-def client(app):
-    return app.test_client()
+def client():
+    flask_app.config['TESTING'] = True
+
+    with flask_app.test_client() as client:
+        yield client
 
 
-def test_index(app, client):
+def test_empty_db(client):
     res = client.get('/')
     assert res.status_code == 200
     expected = {'Hello': 'World'}
     assert expected == json.loads(res.get_data(as_text=True))
 
 
-def login(client, username, password):
-    return client.post('/login', data=dict(
-        username=username,
-        password=password
-    ), follow_redirects=True)
-
+def test_login_logout(client):
+    res = client.get('/login')
+    assert res.status_code == 405
+    res = client.post('/login', json=json.loads(json.dumps({"username": "testuser", "password": "password"})))
+    assert res.status_code == 200
+    response_data = json.loads(res.get_data(as_text=True))
+    token = response_data["Token"]
+    assert response_data
