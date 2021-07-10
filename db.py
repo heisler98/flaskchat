cc9064e5eabd8e11cd247ab1bd43b17ec83c94e1
colingoodman <colin@colingoodman.net>
2021-07-10 09:36:50 -0500

    fix

--- db.py
+++ db.py
@@ -42,9 +42,7 @@ def save_user(username, email, password, fullname):
 
 def store_apn(user_id, token):
     apn_tokens = users_collection.find_one({'_id': ObjectId(user_id)}, {'apn': 1})
-    if type(apn_tokens) == dict:
-        return None
-    if apn_tokens:
+    if type(apn_tokens) == list:
         if token in apn_tokens:
             return None
         else:
