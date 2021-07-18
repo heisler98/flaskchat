503e520a445e2fdfee50da72b721c3175bf78338
colingoodman <colin@colingoodman.net>
2021-07-18 14:04:53 -0500

    fixed crash from bucket_number KeyError

--- db.py
+++ db.py
@@ -190,10 +190,13 @@ def is_room_member(room_id, user_id):
 
 def get_room(room_id):
     room = rooms_collection.find_one({'_id': ObjectId(room_id)})
-    if room['bucket_number']:  # rooms are not instantiated with a bucket number
-        bucket_number = room['bucket_number']
-    else:
+    try:
+        x = room['bucket_number']
+        if x:
+            bucket_number = room['bucket_number']
+    except KeyError as e:
         bucket_number = 0
+
     room_object = Room(room['name'], room_id, room['is_dm'], bucket_number, room['created_by'])
     room_object.set_messages(load_messages(room_id, room.bucket_number))
     return room_object
