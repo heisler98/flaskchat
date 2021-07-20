db177b3650ae8d8b085717540e25491e83b5232a
Colin Goodman <colin@colingoodman.net>
2021-07-20 12:03:00 -0500

    fixing avatars and buckets, cleaning messy code

--- api/sockets.py
+++ api/sockets.py
@@ -149,8 +149,7 @@ def handle_send_message_event(data):
                     apns_targets.extend(user_apn_tokens)
         # room_id, text, sender, bucket_number=0, image_id=None
         handle_apns_load(apns_targets, data)
-        bucket_number = get_latest_bucket_number(room)
-        save_message(room, message, user_id, int(bucket_number), image_id)  # to db
+        save_message(room, message, user_id, iimage_id)  # to db
     else:
         current_app.logger.info("{} not authorized to send to {}".format(username, room))
 
