d21e4e080aa8dcf8753114300a725c4b0f1fc48c
Colin Goodman <colin@colingoodman.net>
2021-07-20 15:31:14 -0500

    patching image issues

--- api/app.py
+++ api/app.py
@@ -25,8 +25,8 @@ def create_app(debug=True):
     app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
     app.config["JWT_HEADER_NAME"] = 'tasty_token'
 
-    # app.config['UPLOAD_FOLDER'] = os.path.abspath(os.path.join('..', 'uploads'))
-    app.config['UPLOAD_FOLDER'] = 'uploads'
+    # app.config['UPLOAD_FOLDER'] = '/tiny/development/uploads' # dev
+    app.config['UPLOAD_FOLDER'] = ' /tiny/flaskchat/uploads' # prod
 
     limiter = Limiter(
         app,
