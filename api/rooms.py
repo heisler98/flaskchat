64db6757591b83ba8c9dad479a9343473780ae21
Colin Goodman <colin@colingoodman.net>
2021-07-23 10:52:28 -0500

    fixed endpoint for changing room attrs

--- api/rooms.py
+++ api/rooms.py
@@ -7,7 +7,7 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from db import get_room_members, get_user, get_user_id, get_messages, is_room_member, get_room, create_dm, find_dm, \
     save_room, get_rooms_for_user, add_room_member, delete_room, is_room_admin, toggle_admin, add_room_members, \
-    get_latest_bucket_number, get_room_admins, add_log_event, room_is_mute, toggle_mute, is_room
+    get_latest_bucket_number, get_room_admins, add_log_event, room_is_mute, toggle_mute, is_room, update_room
 from helper_functions import parse_json
 from model.room import Message
 from model.user import User
@@ -177,11 +177,11 @@ def edit_single_room(room_id):
         return jsonify({'Error': 'Not authorized'}), 403
 
     changeable_values = ['emoji', 'name']
-    for kvp in changed_user:
+    for kvp in changed_room:
         if kvp[0] not in changeable_values:
             return jsonify({'Error': 'You can only edit email, real_name, or username.'}), 400
 
-    update_room(user_id, changed_user)
+    update_room(room_id, changed_room)
     return jsonify({'Success': 'Room modified.'})
 
 
