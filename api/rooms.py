c2706ee56914424651ba2903e1d4deb3719b5417
Hunter Eisler <heisler98@gmail.com>
2021-07-25 23:04:14 -0500

    fixed room name typo

--- api/rooms.py
+++ api/rooms.py
@@ -384,7 +384,7 @@ def return_room(room, username, user_id):
             other_user = get_user(other_id)
             new_name = other_user.username
         elif username in room.name:  # old nomenclature: name concatenation
-            new_name = self.name.replace(username, '')
+            new_name = room.name.replace(username, '')
         else:  # unknown state (perhaps nomenclature changed once more)
             new_name = room.name
     return room.create_personalized_json(new_name)
