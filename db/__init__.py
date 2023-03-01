db80df2dd4a648a4cb3e2934b09a1dbb3e45031b
Hunter Eisler <heisler98@gmail.com>
2023-03-01 05:17:36 -0600

    hunters patches

--- db/__init__.py
+++ db/__init__.py
@@ -464,13 +464,13 @@ def get_latest_bucket_number(room_id):
     return latest_bucket_number
 
 
-def save_message(room_id, text, user_id, image_id=None):
+def save_message(room_id, text, user_id, time_sent, image_id=None):
     if type(room_id) == Room:
         raise TypeError('Using Room object instead of room_id.')
     if type(user_id) == User:
         raise TypeError('Using User object instead of user_id.')
 
-    current_time = time.time()
+    current_time = time_sent
     print('DB: SAVE_MESSAGE', room_id, text, user_id, current_time)
 
     if image_id:
